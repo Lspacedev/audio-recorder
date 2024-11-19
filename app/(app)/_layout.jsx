38db0f523fde2f0f99d46c30053e066818d02a9b
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-11-19 15:57:27 +0200

    removed comments

--- app/(app)/_layout.jsx
+++ app/(app)/_layout.jsx
@@ -11,24 +11,16 @@ export default function AppLayout() {
   useEffect(() => {
     console.log(pathname, session);
   }, [pathname]);
-  // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
     return <Text>Loading...</Text>;
   }
 
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
   if (!session && !authRoutes.includes(pathname)) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
     return <Redirect href={{ pathname: "/sign-in" }} />;
   }
   if (session && authRoutes.includes(pathname)) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
     return <Redirect href={{ pathname: "/home" }} />;
   }
 
-  // This layout can be deferred because it's not the root layout.
   return <Stack />;
 }
